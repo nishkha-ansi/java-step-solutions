4b18cccc11cdf71589ff6b711ed7687e624fedc3
nishkha-ansi <nishkhaansip@gmail.com>
2026-09-17 09:28:20 +0530

    Add Week 3 OOP Java solutions

---
+++ StudentRecord.java
@@ -0,0 +1,17 @@
+class Student {
+    String name;
+    int marks;
+}
+
+public class StudentRecord {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+        student.name = "Divya";
+        student.marks = 88;
+
+        System.out.println("Name: " + student.name +
+                " | Marks: " + student.marks);
+    }
+}
